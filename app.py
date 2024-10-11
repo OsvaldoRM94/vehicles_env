872f452d622e88dc05549ab9d0b21a130faac13f
Valdo94 <osvaldorodriguez1994@hotmail.com>
2024-10-11 16:29:18 -0600

    correccion de identacion

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ if build_histogram: # al hacer clic en el botón
         
             # mostrar un gráfico Plotly interactivo
             st.plotly_chart(fig, use_container_width=True)
-        fig.update_layout(
+            fig.update_layout(
     title_text='Kilometraje de los coches en venta y su frecuencia', # title of plot
     xaxis_title_text='Odómetro', # xaxis label
     yaxis_title_text='Frecuencia', # yaxis label
